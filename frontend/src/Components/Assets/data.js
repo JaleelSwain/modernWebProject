4c01b7fd066c0423fc916bf4f12af41f908d6195
JaleelSwain10 <jaleelswain10@gmail.com>
2025-06-28 18:31:09 -0400

    Popular consoles and offer added

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import soleps5 from './soleps5.jpeg'
+import seriesX from './seriesX.jpeg'
+import ps4pro from './ps4pro.jpeg'
+import seriesS from './seriesS.jpeg'
+
+let data_product = [
+  {
+    id: 1,
+    name: "PlayStation 5",
+    image: soleps5,
+    new_price: 649.99,
+    old_price: 699.99,
+  },
+  {
+    id: 2,
+    name: "Xbox Series X",
+    image: seriesX,
+    new_price: 599.99,
+    old_price: 649.99,
+  },
+  {
+    id: 3,
+    name: "PlayStation 4 Pro ",
+    image: ps4pro,
+    new_price: 449.99,
+    old_price: 499.99,
+  },
+  {
+    id: 4,
+    name: "Xbox Series S",
+    image: seriesS,
+    new_price: 699.00,
+    old_price: 749.00,
+  },
+];
+
+export default data_product;
