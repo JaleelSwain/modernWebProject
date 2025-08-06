45ae11f0ebf4b1da4d47cbb6f988bf01fcba8a63
JaleelSwain10 <jaleelswain10@gmail.com>
2025-08-06 15:40:51 -0400

    added related products to product page

---
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './RelatedProducts.css'
+import data_product from '../Assets/all_products'
+import { Item } from '../Items/Item'
+
+export const RelatedProducts = () => {
+  return (
+    <div className='relatedproduct'>
+        <h1>Related Products</h1>
+        <hr />
+        <div className="relatedproducts-item">
+            {data_product.map((item,i)=>{
+                return <Item key={i} id={item.id} name={item.name} image={item.image} price={item.price} description={item.description}/>
+            })}
+        </div>
+    </div>
+  )
+}
